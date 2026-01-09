f071ea3e395b81a9009c3f9d29cc39452d304954
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2026-01-09 22:16:39 +0000

    Stuff

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,9 +1,4 @@
-using NUnit.Framework;
-using Unity.Collections;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -13,7 +8,7 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject bullet;
     private bool isShooting;
-    private float shootingTime = 0.3f;
+    private float shootingTime = 0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -32,43 +27,42 @@ public class PlayerAttack : MonoBehaviour
     }
     void Shoot()
     {
-        if (isShooting)
+        if (!isShooting)
         {
             if (_PM.facingDirection == Vector2.down)
             {
                 anime.Play("Player_RangeAttack_DOWN");
                 Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y - 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 180));
-                shootingTimer();
+                shootingTime = 0f;
             }
             if (_PM.facingDirection == Vector2.up)
             {
                 anime.Play("Player_RangeAttack_UP");
                 Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y + 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 0));
-                shootingTimer();
+                shootingTime = 0f;
             }
             if (_PM.facingDirection == Vector2.right)
             {
                 anime.Play("Player_RangeAttack_RIGHT");
                 Instantiate(bullet,new Vector2 (Player.transform.position.x + 1 ,Player.transform.position.y),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 270));
-                shootingTimer();
+                shootingTime = 0f;
             }
             if (_PM.facingDirection == Vector2.left)
             {
                 anime.Play("Player_RangeAttack_LEFT");
                 Instantiate(bullet,new Vector2 (Player.transform.position.x - 1,Player.transform.position.y ),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 90));
-                shootingTimer();
+                shootingTime = 0f;
             }
         }
     }
     void shootingTimer()
     {
-        shootingTime = 0.3f;
-        shootingTime -= Time.deltaTime;
-        if (shootingTime >= 0)
+        shootingTime += Time.deltaTime;
+        if (shootingTime >= 0.5)
         {
             isShooting = false;
         }
-        if (shootingTime <= 0)
+        if (shootingTime <= 0.5)
         {
             isShooting = true;
         }
