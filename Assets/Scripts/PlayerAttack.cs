5ed0f74693a6d107dded5aa3524c6fafcad8bcae
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2026-01-09 21:38:32 +0000

    Stuff

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,3 +1,5 @@
+using Unity.Collections;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,6 +9,7 @@ public class PlayerAttack : MonoBehaviour
     private PlayerMovement _PM;
     [SerializeField] private string attackButton = "Fire2";
     private Animator anime;
+    [SerializeField] private GameObject Player;
     [SerializeField] private GameObject bullet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,24 +31,22 @@ public class PlayerAttack : MonoBehaviour
          if (_PM.facingDirection == Vector2.down)
         {
             anime.Play("Player_RangeAttack_DOWN");
-            PShoot();
+            Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y - 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 180));
         }
         if (_PM.facingDirection == Vector2.up)
         {
             anime.Play("Player_RangeAttack_UP");
-            PShoot();
+            Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y + 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 0));
         }
         if (_PM.facingDirection == Vector2.right)
         {
             anime.Play("Player_RangeAttack_RIGHT");
+            Instantiate(bullet,new Vector2 (Player.transform.position.x + 1 ,Player.transform.position.y),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 270));
         }
         if (_PM.facingDirection == Vector2.left)
         {
             anime.Play("Player_RangeAttack_LEFT");
+            Instantiate(bullet,new Vector2 (Player.transform.position.x - 1,Player.transform.position.y ),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 90));
         }
-    }
-    void PShoot()
-    {
-        Resources.Load<GameObject>("Prefab/Bullet");
     }
 }
