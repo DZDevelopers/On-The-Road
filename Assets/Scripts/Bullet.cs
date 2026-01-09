2b625edec03925db0185f53f72be1201cb4665b3
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2026-01-09 22:34:13 +0000

    Add bullet direction and lifetime handling
    Updated Bullet.cs to support setting direction and automatic destruction after a set lifetime. Modified PlayerAttack.cs to set bullet direction upon instantiation. Updated Bullet prefab to include new Bullet script with speed parameter.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float speed = 5f;
+    [SerializeField] float lifeTime = 20f;
+    Rigidbody2D _rb;
 
-    // Update is called once per frame
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetDirection(Vector2 dir)
+    {
+        _rb.linearVelocity = dir.normalized * speed;
     }
 }
--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -32,26 +32,30 @@ public class PlayerAttack : MonoBehaviour
             if (_PM.facingDirection == Vector2.down)
             {
                 anime.Play("Player_RangeAttack_DOWN");
-                Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y - 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 180));
+                GameObject b = Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y - 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 180));
                 shootingTime = 0f;
+                b.GetComponent<Bullet>().SetDirection(Vector2.down);
             }
             if (_PM.facingDirection == Vector2.up)
             {
                 anime.Play("Player_RangeAttack_UP");
-                Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y + 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 0));
+                GameObject b = Instantiate(bullet,new Vector2 (Player.transform.position.x,Player.transform.position.y + 1),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 0));
                 shootingTime = 0f;
+                b.GetComponent<Bullet>().SetDirection(Vector2.up);
             }
             if (_PM.facingDirection == Vector2.right)
             {
                 anime.Play("Player_RangeAttack_RIGHT");
-                Instantiate(bullet,new Vector2 (Player.transform.position.x + 1 ,Player.transform.position.y),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 270));
+                GameObject b = Instantiate(bullet,new Vector2 (Player.transform.position.x + 1 ,Player.transform.position.y),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 270));
                 shootingTime = 0f;
+                b.GetComponent<Bullet>().SetDirection(Vector2.right);
             }
             if (_PM.facingDirection == Vector2.left)
             {
                 anime.Play("Player_RangeAttack_LEFT");
-                Instantiate(bullet,new Vector2 (Player.transform.position.x - 1,Player.transform.position.y ),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 90));
+                GameObject b = Instantiate(bullet,new Vector2 (Player.transform.position.x - 1,Player.transform.position.y ),Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, 90));
                 shootingTime = 0f;
+                b.GetComponent<Bullet>().SetDirection(Vector2.left);
             }
         }
     }
