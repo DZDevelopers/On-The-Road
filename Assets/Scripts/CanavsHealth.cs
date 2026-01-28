8abc9083adc73349b79ea38b6a7812a6747daee4
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2026-01-29 00:01:19 +0100

    Update CanavsHealth.cs

--- Assets/Scripts/CanavsHealth.cs
+++ Assets/Scripts/CanavsHealth.cs
@@ -10,6 +10,10 @@ public class CanavsHealth : MonoBehaviour
     [SerializeField] private Image Health_1;
     [SerializeField] private Image Health_2;
     [SerializeField] private Image Health_3;
+    [SerializeField] private Image Health_4;
+    [SerializeField] private Image Health_5;
+    [SerializeField] private Image Health_6;
+
     [SerializeField] private Sprite FullHealth;
     [SerializeField] private Sprite HalfHealth;
     [SerializeField] private Sprite NoHealth;
@@ -19,12 +23,18 @@ public class CanavsHealth : MonoBehaviour
     [SerializeField] private Image Ammo_3;
     [SerializeField] private Image Ammo_4;
     [SerializeField] private Image Ammo_5;
+    [SerializeField] private Image Ammo_6;
+    [SerializeField] private Image Ammo_7;
+    [SerializeField] private Image Ammo_8;
     [SerializeField] private Sprite FullAmmo;
     [SerializeField] private Sprite NoAmmo;
 
     [SerializeField] private Image Heal_1;
     [SerializeField] private Image Heal_2;
     [SerializeField] private Image Heal_3;
+    [SerializeField] private Image Heal_4;
+    [SerializeField] private Image Heal_5;
+    [SerializeField] private Image Heal_6;
     [SerializeField] private Sprite FullHeal;
     [SerializeField] private Sprite SemiFullHeal;
     [SerializeField] private Sprite HalfFullHeal;
@@ -37,190 +47,533 @@ public class CanavsHealth : MonoBehaviour
     void Update()
     {
         // Health
-        if (_PH.playerHealth == 6)
-        {
-            Health_3.sprite = FullHealth;
-            Health_2.sprite = FullHealth;
-            Health_1.sprite = FullHealth;
-        }
-        if (_PH.playerHealth == 5)
-        {
-            Health_3.sprite = HalfHealth;
-            Health_2.sprite = FullHealth;
-            Health_1.sprite = FullHealth;
-        }
-        if (_PH.playerHealth == 4)
-        {
-            Health_3.sprite = NoHealth;
-            Health_2.sprite = FullHealth;
-            Health_1.sprite = FullHealth;
-        }
-        if (_PH.playerHealth == 3)
-        {
-            Health_3.sprite = NoHealth;
-            Health_2.sprite = HalfHealth;
-            Health_1.sprite = FullHealth;
-        }
-        if (_PH.playerHealth == 2)
-        {
-            Health_3.sprite = NoHealth;
-            Health_2.sprite = NoHealth;
-            Health_1.sprite = FullHealth;
-        }
+    #region
         if (_PH.playerHealth == 1)
         {
-            Health_3.sprite = NoHealth;
-            Health_2.sprite = NoHealth;
-            Health_1.sprite = HalfHealth;
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = NoHealth;
+    Health_3.sprite = NoHealth;
+    Health_2.sprite = NoHealth;
+    Health_1.sprite = HalfHealth;
         }
-
+if (_PH.playerHealth == 2)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = NoHealth;
+    Health_3.sprite = NoHealth;
+    Health_2.sprite = NoHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 3)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = NoHealth;
+    Health_3.sprite = NoHealth;
+    Health_2.sprite = HalfHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 4)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = NoHealth;
+    Health_3.sprite = NoHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 5)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = NoHealth;
+    Health_3.sprite = HalfHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 6)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = NoHealth;
+    Health_3.sprite = FullHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 7)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = HalfHealth;
+    Health_3.sprite = FullHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 8)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = NoHealth;
+    Health_4.sprite = FullHealth;
+    Health_3.sprite = FullHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 9)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = HalfHealth;
+    Health_4.sprite = FullHealth;
+    Health_3.sprite = FullHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 10)
+{
+    Health_6.sprite = NoHealth;
+    Health_5.sprite = FullHealth;
+    Health_4.sprite = FullHealth;
+    Health_3.sprite = FullHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+if (_PH.playerHealth == 11)
+{
+    Health_6.sprite = HalfHealth;
+    Health_5.sprite = FullHealth;
+    Health_4.sprite = FullHealth;
+    Health_3.sprite = FullHealth;
+    Health_2.sprite = FullHealth;
+    Health_1.sprite = FullHealth;
+}
+    if (_PH.playerHealth == 12)
+    {
+        Health_6.sprite = FullHealth;
+        Health_5.sprite = FullHealth;
+        Health_4.sprite = FullHealth;
+        Health_3.sprite = FullHealth;
+        Health_2.sprite = FullHealth;
+        Health_1.sprite = FullHealth;
+    }
+    #endregion
         // Ammo
-        if (_PA.AmmoAmount == 5)
-        {
-            Ammo_5.sprite = FullAmmo;
-            Ammo_4.sprite = FullAmmo;
-            Ammo_3.sprite = FullAmmo;
-            Ammo_2.sprite = FullAmmo;
-            Ammo_1.sprite = FullAmmo;
-        }
-        if (_PA.AmmoAmount == 4)
-        {
-            Ammo_5.sprite = NoAmmo;
-            Ammo_4.sprite = FullAmmo;
-            Ammo_3.sprite = FullAmmo;
-            Ammo_2.sprite = FullAmmo;
-            Ammo_1.sprite = FullAmmo;
-        }
-        if (_PA.AmmoAmount == 3)
-        {
-            Ammo_5.sprite = NoAmmo;
-            Ammo_4.sprite = NoAmmo;
-            Ammo_3.sprite = FullAmmo;
-            Ammo_2.sprite = FullAmmo;
-            Ammo_1.sprite = FullAmmo;
-        }
-        if (_PA.AmmoAmount == 2)
-        {
-            Ammo_5.sprite = NoAmmo;
-            Ammo_4.sprite = NoAmmo;
-            Ammo_3.sprite = NoAmmo;
-            Ammo_2.sprite = FullAmmo;
-            Ammo_1.sprite = FullAmmo;
-        }
-        if (_PA.AmmoAmount == 1)
-        {
-            Ammo_5.sprite = NoAmmo;
-            Ammo_4.sprite = NoAmmo;
-            Ammo_3.sprite = NoAmmo;
-            Ammo_2.sprite = NoAmmo;
-            Ammo_1.sprite = FullAmmo;
-        }
+    #region
         if (_PA.AmmoAmount == 0)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = NoAmmo;
+    Ammo_6.sprite = NoAmmo;
+    Ammo_5.sprite = NoAmmo;
+    Ammo_4.sprite = NoAmmo;
+    Ammo_3.sprite = NoAmmo;
+    Ammo_2.sprite = NoAmmo;
+    Ammo_1.sprite = NoAmmo;
+}
+
+if (_PA.AmmoAmount == 1)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = NoAmmo;
+    Ammo_6.sprite = NoAmmo;
+    Ammo_5.sprite = NoAmmo;
+    Ammo_4.sprite = NoAmmo;
+    Ammo_3.sprite = NoAmmo;
+    Ammo_2.sprite = NoAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+
+if (_PA.AmmoAmount == 2)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = NoAmmo;
+    Ammo_6.sprite = NoAmmo;
+    Ammo_5.sprite = NoAmmo;
+    Ammo_4.sprite = NoAmmo;
+    Ammo_3.sprite = NoAmmo;
+    Ammo_2.sprite = FullAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+
+if (_PA.AmmoAmount == 3)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = NoAmmo;
+    Ammo_6.sprite = NoAmmo;
+    Ammo_5.sprite = NoAmmo;
+    Ammo_4.sprite = NoAmmo;
+    Ammo_3.sprite = FullAmmo;
+    Ammo_2.sprite = FullAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+
+if (_PA.AmmoAmount == 4)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = NoAmmo;
+    Ammo_6.sprite = NoAmmo;
+    Ammo_5.sprite = NoAmmo;
+    Ammo_4.sprite = FullAmmo;
+    Ammo_3.sprite = FullAmmo;
+    Ammo_2.sprite = FullAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+
+if (_PA.AmmoAmount == 5)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = NoAmmo;
+    Ammo_6.sprite = NoAmmo;
+    Ammo_5.sprite = FullAmmo;
+    Ammo_4.sprite = FullAmmo;
+    Ammo_3.sprite = FullAmmo;
+    Ammo_2.sprite = FullAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+
+if (_PA.AmmoAmount == 6)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = NoAmmo;
+    Ammo_6.sprite = FullAmmo;
+    Ammo_5.sprite = FullAmmo;
+    Ammo_4.sprite = FullAmmo;
+    Ammo_3.sprite = FullAmmo;
+    Ammo_2.sprite = FullAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+
+if (_PA.AmmoAmount == 7)
+{
+    Ammo_8.sprite = NoAmmo;
+    Ammo_7.sprite = FullAmmo;
+    Ammo_6.sprite = FullAmmo;
+    Ammo_5.sprite = FullAmmo;
+    Ammo_4.sprite = FullAmmo;
+    Ammo_3.sprite = FullAmmo;
+    Ammo_2.sprite = FullAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+
+if (_PA.AmmoAmount == 8)
+{
+    Ammo_8.sprite = FullAmmo;
+    Ammo_7.sprite = FullAmmo;
+    Ammo_6.sprite = FullAmmo;
+    Ammo_5.sprite = FullAmmo;
+    Ammo_4.sprite = FullAmmo;
+    Ammo_3.sprite = FullAmmo;
+    Ammo_2.sprite = FullAmmo;
+    Ammo_1.sprite = FullAmmo;
+}
+        #endregion
+        // Healing
+    #region
+if (_PA.HealAmount == 0)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = NoHeal;
+    Heal_1.sprite = NoHeal;
+}
+if (_PA.HealAmount == 1)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = NoHeal;
+    Heal_1.sprite = SemiNoHeal;
+}
+if (_PA.HealAmount == 2)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = NoHeal;
+    Heal_1.sprite = HalfNoHeal;
+}
+if (_PA.HealAmount == 3)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = NoHeal;
+    Heal_1.sprite = HalfFullHeal;
+}
+if (_PA.HealAmount == 4)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = NoHeal;
+    Heal_1.sprite = SemiFullHeal;
+}
+if (_PA.HealAmount == 5)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = NoHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 6)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = SemiNoHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 7)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = HalfNoHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 8)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = HalfFullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 9)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = SemiFullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 10)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = NoHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 11)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = SemiNoHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 12)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = HalfNoHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 13)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = HalfFullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 14)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = SemiFullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 15)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = NoHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 16)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = SemiNoHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 17)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = HalfNoHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 18)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = HalfFullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 19)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = SemiFullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 20)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = NoHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 21)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = SemiNoHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 22)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = HalfNoHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 23)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = HalfFullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 24)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = SemiFullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 25)
+{
+    Heal_6.sprite = NoHeal;
+    Heal_5.sprite = FullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 26)
+{
+    Heal_6.sprite = SemiNoHeal;
+    Heal_5.sprite = FullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 27)
+{
+    Heal_6.sprite = HalfNoHeal;
+    Heal_5.sprite = FullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 28)
+{
+    Heal_6.sprite = HalfFullHeal;
+    Heal_5.sprite = FullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 29)
+{
+    Heal_6.sprite = SemiFullHeal;
+    Heal_5.sprite = FullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+if (_PA.HealAmount == 30)
+{
+    Heal_6.sprite = FullHeal;
+    Heal_5.sprite = FullHeal;
+    Heal_4.sprite = FullHeal;
+    Heal_3.sprite = FullHeal;
+    Heal_2.sprite = FullHeal;
+    Heal_1.sprite = FullHeal;
+}
+#endregion
+        Exp.text = $"EXP:{_PA.playerEXP}/{_PA.playerLevel * 50}";
+        if (_PA.AmmoMaxAmount > 5)
         {
-            Ammo_5.sprite = NoAmmo;
-            Ammo_4.sprite = NoAmmo;
-            Ammo_3.sprite = NoAmmo;
-            Ammo_2.sprite = NoAmmo;
-            Ammo_1.sprite = NoAmmo;
+            Ammo_6.gameObject.SetActive(true);
+        }
+        if (_PA.AmmoMaxAmount > 6)
+        {
+            Ammo_7.gameObject.SetActive(true);
+        }
+        if (_PA.AmmoMaxAmount > 7)
+        {
+            Ammo_8.gameObject.SetActive(true);
         }
 
-        // Healing
-        if (_PA.HealAmount == 15)
+        if (_PA.HealMaxAmount > 15)
         {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = FullHeal;
-            Heal_3.sprite = FullHeal;
+            Heal_4.gameObject.SetActive(true);
         }
-        if (_PA.HealAmount == 14)
-        {           
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = FullHeal;
-            Heal_3.sprite = SemiFullHeal;
-        }
-        if (_PA.HealAmount == 13)
+        if (_PA.HealMaxAmount > 20)
         {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = FullHeal;
-            Heal_3.sprite = HalfFullHeal;
-        }   
-        if (_PA.HealAmount == 12)
+            Heal_5.gameObject.SetActive(true);
+        }
+        if (_PA.HealMaxAmount > 25)
         {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = FullHeal;
-            Heal_3.sprite = HalfNoHeal;
+            Heal_6.gameObject.SetActive(true);
         }
-        if (_PA.HealAmount == 11)
-        {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = FullHeal;
-            Heal_3.sprite = SemiNoHeal;
-        }
-        if (_PA.HealAmount == 10)
-        {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = FullHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 9)
-        {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = SemiFullHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 8)
-        {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = HalfFullHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 7)
-        {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = HalfNoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 6)
-        {
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = SemiNoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 5)
-        {   
-            Heal_1.sprite = FullHeal;
-            Heal_2.sprite = NoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 4)
-        {
-            Heal_1.sprite = SemiFullHeal;
-            Heal_2.sprite = NoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 3)
-        {
-            Heal_1.sprite = HalfFullHeal;
-            Heal_2.sprite = NoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 2)
-        {
-            Heal_1.sprite = HalfNoHeal;
-            Heal_2.sprite = NoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount == 1)
-        {
-            Heal_1.sprite = SemiNoHeal;
-            Heal_2.sprite = NoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        if (_PA.HealAmount <= 0)
-        {
-            Heal_1.sprite = NoHeal;
-            Heal_2.sprite = NoHeal;
-            Heal_3.sprite = NoHeal;
-        }
-        Exp.text = $"EXP:{_PA.playerEXP}/{_PA.playerLevel * 50}";
     }
 }
